f6af0856d74f574961c3ef7462867bd757af9232
taulant-eulerity <taulant@eulerity.com>
2022-03-17 11:49:07 -0400

    siplyfy how we redirect

--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -4,13 +4,11 @@ const path = require("path");
 const authenticateToken = (req, res, next) => {
     const token = req.cookies?.accessToken
     if(token == null) {
-        switch(req.path) {
-            case "/admin": return  res.sendFile(path.join(__dirname, "../../", "/admin/authenticate.html"))
-            case "/app": return  res.sendFile(path.join(__dirname, "../../", "/admin/authenticate.html"))
-            default: return res.sendStatus(401)
-        }
+        return req.path?.startsWith('/api') 
+            ? res.sendStatus(401) 
+            : res.sendFile(path.join(__dirname, "../../", "/admin/authenticate.html"))
     }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,_) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, _user) => {
         if(err) {
             res.clearCookie("accessToken");
             res.status(500).json("Server Error: JWS token in invalid, Please Reload!")
