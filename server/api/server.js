c2c2a622ab0a7f30621c30d8006c6ec272ca0fcd
taulant-eulerity <taulant@eulerity.com>
2022-03-31 17:01:56 -0400

    add changes

--- server/api/server.js
+++ server/api/server.js
@@ -10,7 +10,7 @@ const { navigator, userList } = require("../controller/app");
 const path = require("path");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cookieParser());
