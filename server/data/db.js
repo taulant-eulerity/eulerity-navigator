dc91c6e4e2a47cffeb5a7ca535cd521c1ca6ef61
taulant-eulerity <taulant@eulerity.com>
2022-04-01 11:55:50 -0400

    test

--- server/data/db.js
+++ server/data/db.js
@@ -3,7 +3,7 @@ console.log(process.env.DATABASE_URL, 'is there')
 const db = new Sequelize(
     process.env.DATABASE_URL || "postgres://localhost:5432/postgres",
     {
-      ssl: { rejectUnauthorized: false },
+      ssl: true,
       logging: false // unless you like the logs
       // ...and there are many other options you may want to play with
     }
