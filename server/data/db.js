5717b552441ac1ae58994725300bbecaa1777e4f
taulant-eulerity <taulant@eulerity.com>
2022-09-15 14:30:06 -0400

    test

--- server/data/db.js
+++ server/data/db.js
@@ -13,10 +13,6 @@ const db = new Sequelize('postgres', null,
     process.env.DATABASE_URL || "postgres",
     {
       dialect: 'postgres',
-      protocol: 'postgres',
-      username: 'postgres',
-      host: 'ec2-54-157-79-121.compute-1.amazonaws.com',
-      password: 'f1c60dc48dfe658407f16500c2adb9b20a56aef8528fec91cb43752cac118435',
       ...isProd,
       logging: false // unless you like the logs
     }
