2dcf9a1db291ef3e379fc74fd6318e03f9e90fd6
taulant-eulerity <taulant@eulerity.com>
2022-09-15 13:12:46 -0400

    test

--- server/data/db.js
+++ server/data/db.js
@@ -8,6 +8,8 @@ const isProd =  process.env.PROD ? {
     }
   }
 }:{}
+
+console.log(process.env.DATABASE_URL, process.env.PROD, 'XOXOXOXOXOXO')
 const db = new Sequelize('postgres', null,
     process.env.DATABASE_URL || "postgres",
     {
